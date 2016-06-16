584c92b702bcd5e1e7f1fdf87a05bf88ab21868b
chris250690 <chris250690@gmail.com>
2016-06-16 18:37:10 +0200

    Ajout de la fenêtre de login, modification de la fenêtre de démarrage, bloquage des contrôles sensibles

---
+++ EasyPlanner/EasyPlanner/LoginFormWindows.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EasyPlanner
+{
+    /// <summary>
+    /// Logique d'interaction pour LoginFormWindow.xaml
+    /// </summary>
+    /// 
+   
+    public partial class LoginFormWindow : Window
+    {
+        bd_easyplannerEntities bdModel;
+        Person user;
+        Boolean isAdmin = false;
+
+        public LoginFormWindow()
+        {
+            InitializeComponent();
+            bdModel = bd_easyplannerEntities.OpenWithFallback();
+            if (bdModel == null)
+            {
+                //afficher erreur
+                this.Close();
+            }
+        }
+
+        private void btnQuit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUserName.Text==""|| pwPassword.Password == "")
+            {
+                //erreur champ vide
+            }
+            else
+            {
+                List<Person> lsUser = bdModel.People.ToList();
+                Person current;
+                for(int i=0; i < lsUser.Count; i++)
+                {
+                    current = lsUser.ElementAt(i);
+                    if(current.name.ToLower() == txtUserName.Text.ToLower() || current.password == pwPassword.Password)
+                    {
+                        user = current;
+                    }
+                }
+
+                if(user != null)
+                {
+                    //ouvrir main
+                    if(user.idRole == 3)
+                    {
+                        isAdmin = true;
+                    }
+                    new MainWindow(user, isAdmin).Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Nom ou mot de passe incorrect", "Erreur de connexion");
+                }
+            }
+        }
+    }
+}
