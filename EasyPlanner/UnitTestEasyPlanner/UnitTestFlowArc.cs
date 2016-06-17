5540d48f6026ce7b93970c031ce786b33ecee5f1
Plinio Sacchetti <plinio.sacchetti@gmail.com>
2016-06-17 11:10:05 +0200

    UnitTest Class FlowArc

---
+++ EasyPlanner/UnitTestEasyPlanner/UnitTestFlowArc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EasyPlanner;
+
+namespace UnitTestEasyPlanner
+{
+    [TestClass]
+    public class UnitTestFlowArc
+    {
+        //Est-il possible de créer un arc de type FlowArcs
+        [TestMethod]
+        public void ConstructorParamValidesTesting()
+        {
+            FlowNode nodeStart = new FlowNode("Start");
+            FlowNode nodeEnd = new FlowNode("End");
+
+            FlowArc arc = new FlowArc(nodeStart, nodeEnd, 2);
+            Assert.IsNotNull(arc);
+        }
+
+        //Le texte renvoyé correspond
+        [TestMethod]
+        public void ToStringTesting()
+        {
+            FlowNode nodeStart = new FlowNode("Start");
+            FlowNode nodeEnd = new FlowNode("End");
+
+            FlowArc arc = new FlowArc(nodeStart, nodeEnd, 2);
+            Assert.AreEqual("(Start) -> (End)", arc.ToString());
+        }
+    }
+}
